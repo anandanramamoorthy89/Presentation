f205ffe0b6eb7efb4efe166018919fb7ec392d89
anandanramamoorthy89 <166733448+anandanramamoorthy89@users.noreply.github.com>
2024-04-12 15:50:59 +0200

    Update uber.pickups.py

--- uber.pickups.py
+++ uber.pickups.py
@@ -7,5 +7,12 @@ st.sidebar.title("Contents")
 pages = ["Exploration","DataViz","Modelling"]
 pages=st.sidebar.radio("Go to", pages)
 
+import pandas as pd
+import numpy as np
+#read csv and only mark empty cells as NaN so that "None" doesn't vanish
+df=pd.read_csv(r"C:\Users\anand\Downloads\kaggle-survey-2020\kaggle_survey_2020_responses.csv",na_values=[''],keep_default_na=False)
 
+question = df.loc[0]
+df=df.drop(0)
+print(df.columns)
 
